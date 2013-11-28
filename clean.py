0812c87e50f517edce0390c48b3d9834b5c47cce
QianChengLong <qian_cheng_long@163.com>
2013-11-28 22:27:10 +0800

    refactor

---
+++ clean.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python2
+# coding=utf-8
+
+import sqlite3
+import os
+
+# 变量定义
+db_name = "E:\Programming\Language\Python\Code\my\Todo\My.s3db" # 数据库名称
+Question_fileName = r"E:\Question" # 待解决文件名
+Question_tableName = "TB_Question"
+
+# 参数：db_name，保存的数据库
+#       table_name, 表名
+#       filename, 要整理的文件名
+def clean(db_name, table_name, filename):
+    # 检查文件是否存在
+    if os.path.isfile(filename) == False :
+        exit(-1)
+
+    # 数据库操作
+    db = sqlite3.connect(db_name)
+    c = db.cursor()
+
+    # 读取文件内容
+    file = open(filename, 'r')
+    lines = file.readlines()
+    update_lines = ""
+
+    for line in lines :
+        line = line.strip('\n')
+        fields = line.split()
+        if  len(fields) == 3 :
+            c.execute("insert into %s values('%s', '%s', '%s', NULL)"
+                    % (table_name, fields[0], fields[1], fields[2]))
+        elif len(fields) == 4 :
+            c.execute("insert into %s values('%s', '%s', '%s', '%s')"
+                    % (table_name, fields[0], fields[1], fields[2], fields[3]))
+        else :
+            update_lines += line + '\n'
+
+    file.close()
+    file = open(filename, 'w')
+    file.writelines(update_lines)
+    file.close()
+
+    db.commit()
+    db.close()
---
+++ main.py
@@ -0,0 +1,83 @@
+#! /bin/env python2
+# coding=utf-8
+
+import clean
+import ConfigParser,platform,md5
+
+configfile = "clean.ini"
+db_name = "clean.db"
+todo_tablename = "TB_Todo"
+question_tablename = 'TB_Question'
+
+# 功能：根据操作系统，读取clean.ini，返回相应待处理的文件名
+def getFilenames():
+    todo = ""
+    question = ""
+
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read(configfile)
+    except (IOError, OSError):
+        sys.stderr.write("Error opening config file " + str(configfile))
+        sys.exit(1)
+
+    configSections = config.sections()
+    for sec in configSections:
+        if platform.system() == sec:
+            todo = config.get(sec, 'todo')
+            question = config.get(sec, 'question')
+
+    return (todo, question)
+
+# 功能：判断该文件是否修改过，返回真，说明修改过
+def fileWasModified(filename):
+    file = open(filename, 'r')
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read(configfile)
+    except (IOError, OSError):
+        sys.stderr.write("Error opening config file " + str(configfile))
+        sys.exit(1)
+
+    configSections = config.sections()
+    for sec in configSections:
+        if platform.system() == sec:
+            try:
+                old_md5 = config.get(sec, filename + '_md5', '1')
+            except ConfigParser.NoOptionError:
+                old_md5 = '1'
+
+    return md5.new(file.read()).hexdigest() != old_md5
+
+# 功能：保存文件md5
+def saveMD5(filename):
+    file = open(filename, 'r')
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read(configfile)
+    except (IOError, OSError):
+        sys.stderr.write("Error opening config file " + str(configfile))
+        sys.exit(1)
+    config.set(platform.system(), filename + '_md5', md5.new(file.read()).hexdigest())
+    config.set(platform.system(), "a", "a")
+    config.write(open(configfile, "w"))
+
+def main():
+    # 获得相应平台要处理的文件名
+    (todo_filename, question_filename) = getFilenames()
+    # 检查文件是否修改过
+    if fileWasModified(todo_filename):
+        clean.clean(db_name, todo_tablename, todo_filename)
+        saveMD5(todo_filename)
+        print("clean todo up!")
+    else:
+        print("todo is clean!")
+    if fileWasModified(question_filename):
+        clean.clean(db_name, question_tablename, question_filename)
+        saveMD5(question_filename)
+        print("clean question up!")
+    else:
+        print("question is clean!")
+
+if __name__ == "__main__":
+    main()
